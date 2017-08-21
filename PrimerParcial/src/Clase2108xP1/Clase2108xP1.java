55b55ceb2ec3532c3b490dbd05c519db0e96e0d3
DX94-Quas <dx94.manage@gmail.com>
2017-08-21 18:57:20 -0400

    Correcciones esteticas

--- PrimerParcial/src/Clase2108xP1/Clase2108xP1.java
+++ PrimerParcial/src/Clase2108xP1/Clase2108xP1.java
@@ -7,30 +7,35 @@ public class Clase2108xP1
 
     public static void main(String[] args)
     {
-//        Tiempo t1 = new Tiempo(16, 20, 8);
-//        Tiempo t2 = new Tiempo(16, 20, 10);
-//
-//        System.out.println(t1);
-//        System.out.println(t1);
-//
-//        if (t1.menor(t2))
-//        {
-//            System.out.println("Es Menor");
-//        }
-//        else
-//        {
-//            System.out.println("NO es menor");
-//        }
-//        
-//        if (t1.equals(t2))
-//        {
-//            System.out.println("Son Iguales");
-//        }
-//        else
-//        {
-//            System.out.println("NO son Iguales");
-//        }
+        //Variables
+        Tiempo t1 = new Tiempo(16, 20, 8);
+        Tiempo t2 = new Tiempo(16, 20, 10);
 
+        //Vista
+        System.out.println(t1);
+        System.out.println(t1);
+
+        //Operaciones
+        if (t1.menor(t2))
+        {
+            System.out.println("Es Menor");
+        }
+        else
+        {
+            System.out.println("NO es menor");
+        }
+
+        if (t1.equals(t2))
+        {
+            System.out.println("Son Iguales");
+        }
+        else
+        {
+            System.out.println("NO son Iguales");
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        //Variables
         Racional r1 = new Racional(2, 3);
         Racional r2 = new Racional(5, 6);
 
