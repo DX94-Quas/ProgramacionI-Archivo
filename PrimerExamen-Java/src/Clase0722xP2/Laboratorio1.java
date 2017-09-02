3bf191783f597f83f986c8eed4a71313f750b05e
DX94-Quas <dx94.manage@gmail.com>
2017-09-01 22:28:17 -0400

    Modificacion de Orden / Preparacion para separacion por examenes

---
+++ PrimerExamen-Java/src/Clase0722xP2/Laboratorio1.java
@@ -0,0 +1,176 @@
+// Para Info. de Licencias refiérase al archivo LICENSE ubicado
+// donde estan contenidos todos los proyectos
+
+package Clase0722xP2;
+
+public class Laboratorio1 {
+    
+    public static void main(String[] args)
+    {
+        // LABORATORIO 1
+        System.out.println(soloLetrasMay("ABCD"));
+        System.out.println(soloLetrasMin("ABCD"));
+        System.out.println(cantLetras("3333++++ABCDefgh"));
+        System.out.println(cantDigitos("3333++++A777BCDefgh"));
+        System.out.println(puroDigitos("5684849"));
+        System.out.println(alfaNumerico("5684++"));
+        System.out.println(generarCad("abc",5));
+        System.out.println(palindrome("madam"));
+        System.out.println(extraerDigito("madam777888"));
+        System.out.println(intercalarChar("abc","123"));
+        System.out.println(separarCharConString("abc","123"));
+        // LABORATORIO 1 - Docente
+        System.out.println(intercalarCharDoc("abdvcgvc","1234"));
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    // LABORATORIO 1
+    public static boolean soloLetrasMay(String S1)
+    {
+        int i = 0;
+        while (i < S1.length())
+        {
+            if (!Character.isUpperCase(S1.charAt(i))) return false;
+            i=i+1;
+        }
+        return true;
+    }
+    public static boolean soloLetrasMin(String S1)
+    {
+        int i = 0;
+        while (i < S1.length())
+        {
+            if (!Character.isLowerCase(S1.charAt(i))) return false;
+            i=i+1;
+        }
+        return true;
+    }
+    public static int cantLetras(String S1)
+    {
+        int i = 0;
+        int k =0;
+        while (i < S1.length())
+        {
+            if (Character.isLetter(S1.charAt(i))) k=k+1;
+            i++;
+        }
+        return k;
+    }
+    public static int cantDigitos(String S1)
+    {
+        int i = 0;
+        int k =0;
+        while (i < S1.length())
+        {
+            if (Character.isDigit(S1.charAt(i))) k=k+1;
+            i++;
+        }
+        return k;
+    }
+    public static boolean puroDigitos(String S1)
+    {
+        int i = 0;
+        while (i < S1.length())
+        {
+            if (!Character.isDigit(S1.charAt(i))) return false;
+            i=i+1;
+        }
+        return true;
+    }
+    public static boolean alfaNumerico(String S1)
+    {
+        int i = 0;
+        while (i < S1.length())
+        {
+            if (!Character.isLetterOrDigit(S1.charAt(i))) return false;
+            i=i+1;
+        }
+        return true;
+    }
+    public static String generarCad(String S1, int n)
+    {
+        int i=0;
+        String S2 = "";
+        while (i<n)
+        {
+            S2 = S2.concat(S1);
+            i++;
+        }
+        return S2;
+    }
+    public static boolean palindrome (String S1)
+    {
+        int i =0;
+        int n = S1.length()-1;
+        while (i < n)
+        {
+            if(S1.charAt(i)!=S1.charAt(n-i)) return false;
+            i++;
+        }
+        return true;
+    }
+    public static String extraerLetra(String S1)
+    {
+        int i=0;
+        String S2="";
+        while(i<S1.length())
+        {
+            if (Character.isLetter(S1.charAt(i))) S2=S2+S1.charAt(i);
+            i++;
+        }
+        return S2; 
+    }
+    public static String extraerDigito(String S1)
+    {
+        int i=0;
+        String S2="";
+        while(i<S1.length())
+        {
+            if (Character.isDigit(S1.charAt(i))) S2=S2+S1.charAt(i);
+            i++;
+        }
+        return S2; 
+    }
+    public static String intercalarChar(String S1, String S2)
+    {
+        String S3 = "";
+        int a = S1.length();
+        int b = S2.length();
+        int i =0;
+        int j =0;
+        while (i < a || j<b)
+        {
+            S3 = S3 + S1.charAt(i) + S2.charAt(j);
+            i++;
+            j++;     
+        }
+        return S3;        
+    }
+    public static String separarCharConString(String S1, String S2)
+    {
+        String S3 = "";
+        int a = S1.length();
+        int i =0;
+        while (i < a)
+        {
+            if (i==a-1) S3 = S3 + S1.charAt(i);
+            else S3 = S3 + S1.charAt(i) + S2;
+            i++;     
+        }
+        return S3;        
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    /// IntercalarChar de Docente
+    public static String intercalarCharDoc(String S1, String S2)
+    {
+        String S3="";
+        int i =0;
+        while (i<S1.length() || i < S2.length())
+        {
+            if(i<S1.length()) S3 = S3 + S1.charAt(i);
+            if(i<S2.length()) S3 = S3 + S2.charAt(i);
+            i++;
+        }
+        return S3;
+    }
+    
+}
---
+++ PrimerExamen-Java/src/Clase0724xP1/Laboratorio4.java
@@ -0,0 +1,182 @@
+// Para Info. de Licencias refiérase al archivo LICENSE ubicado
+// donde estan contenidos todos los proyectos
+package Clase0724xP1;
+
+import static Tarea1.Tarea1.charIguales;
+import static Tarea1.Tarea1.puroLetras;
+import static Tarea1.Tarea1.alfaNumerico;
+import static Tarea1.Tarea1.sinVocales;
+import static Tarea1.Tarea1.charDiferentes;
+
+public class Laboratorio4
+{
+
+    public static void main(String[] args)
+    {
+        mostrarLongitud("123334566678999", 5);
+        mostrarIguales("123334566678999");
+        mostrarPalindromes("123334madam5666racecar78999");
+        mostrarLetras("123334madam5666racecar78999");
+        mostrarLetrasDigitos("1///2333+++4madam56...66ra   cecar###78999");
+        mostrarSinVocales("123334madam5666racecar###78999");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Lab 4
+    public static void mostrarLongitud(String S1, int k)
+    {
+        int i = 0;
+        String S2;
+        while (i < S1.length())
+        {
+            int j = i + 1;
+            while (j <= S1.length())
+            {
+                S2 = S1.substring(i, j);
+                if (S2.length() == k)
+                {
+                    System.out.println(S2);
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+
+    public static void mostrarIguales(String S1)
+    {
+        int i = 0;
+        String S2;
+        while (i < S1.length())
+        {
+            int j = i + 1;
+            while (j <= S1.length())
+            {
+                S2 = S1.substring(i, j);
+                if (charIguales(S2) == true)
+                {
+                    System.out.println(S2);
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+
+    public static void mostrarPalindromes(String S1)
+    {
+        int i = 0;
+        String S2;
+        while (i < S1.length())
+        {
+            int j = i + 1;
+            while (j <= S1.length())
+            {
+                S2 = S1.substring(i, j);
+                if (palindrome(S2) == true)
+                {
+                    System.out.println(S2);
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+
+    public static void mostrarLetras(String S1)
+    {
+        int i = 0;
+        String S2;
+        while (i < S1.length())
+        {
+            int j = i + 1;
+            while (j <= S1.length())
+            {
+                S2 = S1.substring(i, j);
+                if (puroLetras(S2) == true)
+                {
+                    System.out.println(S2);
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+
+    public static void mostrarLetrasDigitos(String S1)
+    {
+        int i = 0;
+        String S2;
+        while (i < S1.length())
+        {
+            int j = i + 1;
+            while (j <= S1.length())
+            {
+                S2 = S1.substring(i, j);
+                if (alfaNumerico(S2) == true)
+                {
+                    System.out.println(S2);
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+
+    public static void mostrarSinVocales(String S1)
+    {
+        int i = 0;
+        String S2;
+        while (i < S1.length())
+        {
+            int j = i + 1;
+            while (j <= S1.length())
+            {
+                S2 = S1.substring(i, j);
+                if (sinVocales(S2) == true)
+                {
+                    System.out.println(S2);
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+
+    public static void mostrarDiferentes(String S1)
+    {
+        int i = 0;
+        String S2;
+        while (i < S1.length())
+        {
+            int j = i + 1;
+            while (j <= S1.length())
+            {
+                S2 = S1.substring(i, j);
+                if (charDiferentes(S2) == true)
+                {
+                    System.out.println(S2);
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Librerias Verga
+    public static boolean palindrome(String S1)
+    {
+        int i = 0;
+        int n = S1.length() - 1;
+        while (i < n)
+        {
+            if (S1.charAt(i) != S1.charAt(n - i))
+            {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
